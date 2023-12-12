fadc20049848584e9eb883ff2b846f4a241a145e
aneesh2usman <aneeshplusone@gmail.com>
2023-12-12 13:07:17 +0530

    desktop sqllite

--- lib/lesson3/db_helper/db_helper.dart
+++ lib/lesson3/db_helper/db_helper.dart
@@ -1,25 +1,48 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:io' as io;
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DBHelper {
   static const todo = 'todo';
 
   static Future<Database> database() async {
-    final dbPath = await getDatabasesPath();
-    return await openDatabase(
-      join(dbPath, "$todo.db"),
-      onCreate: (db, version) {
-        db.execute("CREATE TABLE IF NOT EXISTS $todo(id TEXT PRIMARY KEY ,"
-            " title TEXT,"
-            " description TEXT,"
-            " date TEXT)");
-      },
+    sqfliteFfiInit();
+
+    var databaseFactory = databaseFactoryFfi;
+    final io.Directory appDocumentsDir =
+        await getApplicationDocumentsDirectory();
+    String dbPath = join(appDocumentsDir.path, "databases", "todo.db");
+    print("************dbPath********");
+    print(dbPath);
+    var db = await databaseFactory.openDatabase(
+      dbPath,
     );
+    // return await openDatabase(
+    //   join(dbPath, 'todo.db'),
+    //   onCreate: (db, version) {
+    //     db.execute("CREATE TABLE IF NOT EXISTS $todo(id TEXT PRIMARY KEY ,"
+    //         " title TEXT,"
+    //         " description TEXT,"
+    //         " date TEXT)");
+    //   },
+    //   version: 1,
+    // );
+    await db.execute("CREATE TABLE IF NOT EXISTS $todo(id TEXT PRIMARY KEY ,"
+        " title TEXT,"
+        " description TEXT,"
+        " date TEXT)");
+    return db;
   }
 
   static Future selectAll(String table) async {
     final db = await DBHelper.database();
     //With out Query
+    print("*************db.query(table)**********");
+    print(db.query(table));
     return db.query(table);
     // with Query
     // return db.rawQuery("SELECT * FROM $todo");
@@ -55,5 +78,19 @@ class DBHelper {
     final db = await DBHelper.database();
 
     return db.rawQuery('DELETE FROM ${tableName}');
+  }
+
+  static Future deleteById(
+    String tableName,
+    String columnName,
+    String id,
+  ) async {
+    final db = await DBHelper.database();
+
+    return db.delete(
+      tableName,
+      where: '$columnName = ?',
+      whereArgs: [id],
+    );
   }
 }
--- lib/lesson3/show_todo_screen.dart
+++ lib/lesson3/show_todo_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sample/lesson3/add_todo_screen.dart';
+import 'package:sample/lesson3/model/todo_model.dart';
 import 'package:sample/lesson3/providers/todo_provider.dart';
 
 AppBar createAppBar(BuildContext context, TodoProvider todoP) {
@@ -50,7 +51,195 @@ class _ShowTodoScreen extends State<ShowTodoScreen> {
         ),
         backgroundColor: Colors.white,
         appBar: createAppBar(context, todoP),
-        body: null);
+        body: const TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return FutureBuilder(
+      future: Provider.of<TodoProvider>(context, listen: false).selectData(),
+      builder: (context, snapShot) {
+        if (snapShot.connectionState == ConnectionState.done) {
+          return Consumer<TodoProvider>(
+            builder: (context, todoProvider, child) {
+              return todoProvider.todoItem.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: todoProvider.todoItem.length,
+                      itemBuilder: (context, index) {
+                        final helperValue = todoProvider.todoItem[index];
+                        return Dismissible(
+                          key: ValueKey(helperValue.id),
+                          background: const DismissBackGround(
+                            color: Colors.green,
+                            alignment: Alignment.centerLeft,
+                            iconData: Icons.edit_outlined,
+                          ),
+                          secondaryBackground: const DismissBackGround(
+                            color: Colors.red,
+                            alignment: Alignment.centerRight,
+                            iconData: Icons.delete_forever_outlined,
+                          ),
+                          confirmDismiss: (DismissDirection direction) async {
+                            if (direction == DismissDirection.startToEnd) {
+                              // return Navigator.of(context).push(
+                              //   MaterialPageRoute(
+                              //     builder: (context) => EditTodoScreen(
+                              //       id: helperValue.id,
+                              //       title: helperValue.title,
+                              //       description: helperValue.description,
+                              //       date: helperValue.date,
+                              //     ),
+                              //   ),
+                              // );
+                            } else {
+                              showDialog(
+                                useSafeArea: true,
+                                context: context,
+                                builder: (context) =>
+                                    DeleteDialog(helperValue: helperValue),
+                              );
+                            }
+                          },
+                          child: ListViewItemWidget(
+                            helperValue: helperValue,
+                          ),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.indeterminate_check_box_outlined,
+                            size: width * 0.2,
+                          ),
+                          const Text(
+                            'List is empty',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 35,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
+
+class DeleteDialog extends StatelessWidget {
+  const DeleteDialog({
+    Key? key,
+    required this.helperValue,
+  }) : super(key: key);
+
+  final TodoModel helperValue;
+
+  @override
+  Widget build(BuildContext context) {
+    final todoProvider = Provider.of<TodoProvider>(context, listen: false);
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return AlertDialog(
+      scrollable: true,
+      title: const Text('Delete'),
+      content: const Text('Do you want to delete it?'),
+      actions: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(width * 0.02),
+                child: ElevatedButton(
+                  onPressed: () {
+                    todoProvider.deleteById(helperValue.id);
+                    todoProvider.todoItem.remove(helperValue);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Yes'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(width * 0.02),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'No',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class ListViewItemWidget extends StatelessWidget {
+  const ListViewItemWidget({
+    Key? key,
+    required this.helperValue,
+  }) : super(key: key);
+
+  final TodoModel helperValue;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(width * 0.01),
+      ),
+      color: Colors.white,
+      margin: EdgeInsets.symmetric(
+        horizontal: width * 0.03,
+        vertical: height * 0.01,
+      ),
+      elevation: 3,
+      child: ListTile(
+        style: ListTileStyle.drawer,
+        title: Text(helperValue.title),
+        subtitle: Text(
+          helperValue.description,
+          style: TextStyle(
+            color: Colors.grey[600],
+          ),
+        ),
+        trailing: Text(
+          helperValue.date,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
 
@@ -110,4 +299,38 @@ Row PopupYesNoButtonRaw(
       ),
     ],
   );
+}
+
+class DismissBackGround extends StatelessWidget {
+  final Color color;
+  final Alignment alignment;
+  final IconData iconData;
+
+  const DismissBackGround({
+    Key? key,
+    required this.color,
+    required this.alignment,
+    required this.iconData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Container(
+      margin: EdgeInsets.all(width * 0.02),
+      padding: EdgeInsets.all(width * 0.03),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(width * 0.03),
+      ),
+      alignment: alignment,
+      child: Icon(
+        iconData,
+        color: Colors.white,
+      ),
+      height: height * 0.02,
+      width: width,
+    );
+  }
 }
